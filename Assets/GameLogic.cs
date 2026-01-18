9fd4931309cf4ca668b403ef8b83b8940b7fec6c
Ethan Gonzalez <112298070+EthanGon@users.noreply.github.com>
2026-01-18 16:07:08 -0500

    Setting up dictionary for finding place value of number

--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -8,9 +9,20 @@ public class GameLogic : MonoBehaviour
     public TextMeshProUGUI moneyCounter;
     public double moneyPerSec;
     public double placeValueOfMoney;
+    public Dictionary<int, string> placeLogValues;
+    public double numFormatted;
+    public string[] placeValues = { "Thousand", "Million"};
 
     private void Awake()
     {
+        placeLogValues = new Dictionary<int, string>();
+        
+        InitPlaceValues();
+        
+
+
+
+
         instance = this;
         moneyCounter.text = "MONEY\n" + moneyCount.ToString("F3");
     }
@@ -40,11 +52,30 @@ public class GameLogic : MonoBehaviour
 
     public void PrintPlaceValue()
     {
-        double tenthPower = Mathf.Floor(Mathf.Log10((float)moneyCount));
+        double tenthPower = Mathf.Floor(Mathf.Log10((float)moneyCount)); // round down to prevent something like 99 being considers 100th place
         double place = Mathf.Pow(10, (float)tenthPower);
         placeValueOfMoney = place;
+
+        numFormatted = moneyCount / place;
     }
 
+    // when number gets to million and above then you should to format based on value
 
+    public void InitPlaceValues()
+    {
+        int num = 3;
+
+        for (int i = 0; i < placeValues.Length; i++ )
+        {
+            placeLogValues.Add(num, placeValues[i]);
+            num += 3;
+        }
+
+        //Log Dictionary
+        foreach (KeyValuePair<int, string> pair in placeLogValues)
+        {
+            Debug.Log(pair.Key + " : " + pair.Value);
+        }
+    }
 
 }
