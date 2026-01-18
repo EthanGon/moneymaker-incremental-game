6ac91e7756aca6f50e6efbf6700060dbdf9c762b
Ethan Gonzalez <112298070+EthanGon@users.noreply.github.com>
2026-01-17 22:41:10 -0500

    Made Method for getting place value

--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -6,17 +6,43 @@ public class GameLogic : MonoBehaviour
     public static GameLogic instance;
     public double moneyCount;
     public TextMeshProUGUI moneyCounter;
+    public double moneyPerSec;
+    public double placeValueOfMoney;
 
     private void Awake()
     {
         instance = this;
-        moneyCounter.text = "MONEY\n" + moneyCount.ToString();
+        moneyCounter.text = "MONEY\n" + moneyCount.ToString("F3");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+        if (moneyCount < double.MaxValue)
+        {
+            AddMoney(moneyPerSec);
+        }
+        PrintPlaceValue();
+    }
+
+    public void AddMoney(double mps)
+    {
+        moneyCount += mps * Time.deltaTime;
+        moneyCounter.text = "MONEY\n" + moneyCount.ToString("F0");
+    }
+
+
+    public void MoneyClick()
+    {
+        moneyCount++;
+        moneyCounter.text = "MONEY\n" + moneyCount.ToString("F3");
+    }
+
+    public void PrintPlaceValue()
+    {
+        double tenthPower = Mathf.Floor(Mathf.Log10((float)moneyCount));
+        double place = Mathf.Pow(10, (float)tenthPower);
+        placeValueOfMoney = place;
     }
 
 
